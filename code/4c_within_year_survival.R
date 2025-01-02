a168a3e2ed0304b0eca55d5bf35cffb12709f36c
Robin Hinks <robin.e.hinks@gmail.com>
2025-01-01 19:18:48 -0800

    working on plotting survival curves

--- code/4c_within_year_survival.R
+++ code/4c_within_year_survival.R
@@ -20,18 +20,18 @@ setwd(here::here("code"))
 onset_of_breeding_surv <- read_csv(here::here("data", "onset_of_breeding.csv"))
 
 # scaling covariates
-scaled_within_year <- onset_of_breeding_surv %>% 
-  mutate(
-    BRDYEAR_scaled = as.vector(scale(BRDYEAR)),
-    yearly_rain_scaled = as.vector(scale(yearly_rain)),
-    rain_to_date_scaled = as.vector(scale(rain_to_date)),
-    water_flow = as.factor(water_flow),
-    water_regime = as.factor(water_regime), 
-    LocationID = as.factor(LocationID),
-    Watershed = as.factor(Watershed),
-    cum_sun_hours_scaled = as.vector(scale(cum_sun_hours)),
-    dir_dur_scaled = as.vector(scale(dir_dur))) %>% 
-  select(-NumberofEggMasses)
+# scaled_within_year <- onset_of_breeding_surv %>% 
+#   mutate(
+#     BRDYEAR_scaled = as.vector(scale(BRDYEAR)),
+#     yearly_rain_scaled = as.vector(scale(yearly_rain)),
+#     rain_to_date_scaled = as.vector(scale(rain_to_date)),
+#     water_flow = as.factor(water_flow),
+#     water_regime = as.factor(water_regime), 
+#     LocationID = as.factor(LocationID),
+#     Watershed = as.factor(Watershed),
+#     cum_sun_hours_scaled = as.vector(scale(cum_sun_hours)),
+#     dir_dur_scaled = as.vector(scale(dir_dur))) %>% 
+#   select(-NumberofEggMasses)
 
 # for unscaled data
 unscaled_within_year <- onset_of_breeding_surv %>% 
@@ -43,12 +43,19 @@ unscaled_within_year <- onset_of_breeding_surv %>%
   select(-NumberofEggMasses)
 
 # creating a "complete case" column
-scaled_within_year$complete_case <- complete.cases(scaled_within_year)
-complete_onset <- scaled_within_year %>% filter(complete_case == TRUE) %>% select(-complete_case)
+# scaled_within_year$complete_case <- complete.cases(scaled_within_year)
+# complete_onset <- scaled_within_year %>% filter(complete_case == TRUE) %>% select(-complete_case)
 
 # run only these lines to prep data for unscaled models
 unscaled_within_year$complete_case <- complete.cases(unscaled_within_year)
 complete_onset <- unscaled_within_year %>% filter(complete_case == TRUE) %>% select(-complete_case)
+
+# testing collinearity between day of year and sun hours
+sun_lm <- lm(cum_sun_hours ~ dayOfWY, data = complete_onset)
+sun_resid <- resid(sun_lm)
+
+complete_onset <- complete_onset %>% 
+  mutate(sun_resid = sun_resid)
 #### *** SURVIVAL MODELS *** ####
 
 #intercept model of the mean
@@ -103,15 +110,16 @@ ggsurvplot(
 # no random effects: rain to date + cumulative sun hours
 cox_model_no_random <- coxph(Surv(dayOfWY, breeding_status) ~ 
                      rain_to_date + 
-                     cum_sun_hours + 
-                     strata(Watershed), 
+                     # cum_sun_hours + 
+                     sun_resid, 
                    data = complete_onset)
 summary(cox_model_no_random)
 
 # frailty effect: rain to date + cumulative sun hours
 cox_model_frailty <- coxph(Surv(dayOfWY, breeding_status) ~ 
-                     rain_to_date_scaled + 
-                     cum_sun_hours_scaled +
+                     rain_to_date + 
+                     # cum_sun_hours +
+                     sun_resid +
                      frailty(LocationID), 
                    data = complete_onset)
 summary(cox_model_frailty)
@@ -119,7 +127,9 @@ summary(cox_model_frailty)
 # coxme with random effects: rain to date + cumulative sun hours
 coxme_model <- coxme(Surv(dayOfWY, breeding_status) ~ 
                      rain_to_date +
-                     cum_sun_hours +
+                     sun_resid +
+                     # cum_sun_hours +
+                     # (1 | Watershed),
                      (1 | LocationID),
                    data = complete_onset)
 
@@ -136,9 +146,41 @@ plot(test_cox)
 
 
 # trying to plot survival curves... not working very well atm :(
-ggplot(survfit(cox_model_no_random))
 
+# extract fixed effects and random effects of coxme model
+fixed_effects <- fixef(coxme_model)
+random_effects <- ranef(coxme_model)$LocationID
 
+# get linear predictors of coxme model
+linear_pred <- predict(coxme_model, type = "lp")
+
+# get baseline survival curve using cox_model_no_random results
+baseline_surv <- survfit(cox_model_no_random)
+
+# modify survival curve for each LocationID
+surv_summary <- summary(baseline_surv, times = baseline_surv$time)
+survival_estimates <- outer(
+  surv_summary$surv,
+  exp(random_effects),
+  function(base, re) base^re
+)
+
+# generate data for plotting
+plot_data <- data.frame(
+  dayOfWY = rep(surv_summary$time, each = length(random_effects)),
+  survival = as.vector(survival_estimates),
+  random_effect = rep(names(random_effects), length(surv_summary$time))
+) # for future reference: dayOfWY only goes up to 180 because that's the last day of first breeding
+
+# plot!
+ggplot(plot_data, aes(x = dayOfWY, y = survival, color = random_effect)) +
+  geom_line() +
+  labs(
+    x = "Day of Water Year",
+    y = "Survival Probability",
+    color = "LocationID",
+    title = "Survival Curves by LocationID (Random Effect)"
+  ) + theme_minimal()
 
 #### plot model -- forest plot (this one works!) ####
 
