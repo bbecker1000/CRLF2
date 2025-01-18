7d4758f026033ec2b55d774d043a439875002a4e
Robin Hinks <robin.e.hinks@gmail.com>
2025-01-18 13:34:27 -0800

    trying to troubleshoot within year plots, not working right now

--- code/4c_within_year_survival.R
+++ code/4c_within_year_survival.R
@@ -47,18 +47,18 @@ unscaled_within_year <- onset_of_breeding_surv %>%
 
 # run only these lines to prep data for unscaled models
 unscaled_within_year$complete_case <- complete.cases(unscaled_within_year)
-complete_onset <- unscaled_within_year %>% filter(complete_case == TRUE) %>% select(-complete_case)
+complete_onset <- unscaled_within_year %>% filter(complete_case == TRUE) %>% select(-complete_case) %>% 
+  mutate(time2 = dayOfWY + 1)
 
 # testing collinearity between day of year and sun hours
 sun_lm <- lm(cum_sun_hours ~ dayOfWY, data = complete_onset)
 sun_resid <- resid(sun_lm)
 
-complete_onset <- complete_onset %>% 
-  mutate(sun_resid = sun_resid) %>% 
-  group_by(LocationID, dayOfWY) %>% 
-  mutate(breeding_status = max(breeding_status))
-
-complete_onset <- distinct(complete_onset, diff = paste(LocationID, dayOfWY), .keep_all = "true")
+complete_onset <- complete_onset %>%
+  mutate(sun_resid = sun_resid)
+#   mutate(breeding_status = max(breeding_status))
+# 
+# complete_onset <- distinct(complete_onset, diff = paste(LocationID, dayOfWY), .keep_all = "true")
 
 
 #### *** SURVIVAL MODELS *** ####
@@ -113,29 +113,31 @@ ggsurvplot(
 
 ## choose one
 # no random effects: rain to date + cumulative sun hours
-cox_model_no_random <- coxph(Surv(dayOfWY, breeding_status) ~ 
-                     rain_to_date +
+cox_model_no_random <- coxph(Surv(dayOfWY, time2, breeding_status) ~
+                     rain_to_date,
                      # cum_sun_hours + 
-                     sun_resid,
+                     # sun_resid,
                    data = complete_onset)
 summary(cox_model_no_random)
 
+plot(survfit(cox_model_no_random))
+
+
 # frailty effect: rain to date + cumulative sun hours
-cox_model_frailty <- coxph(Surv(dayOfWY, breeding_status) ~ 
+cox_model_frailty <- coxph(Surv(dayOfWY, time2, breeding_status) ~ 
                      rain_to_date + 
                      # cum_sun_hours +
-                     sun_resid +
+                     # sun_resid +
                      frailty(LocationID), 
                    data = complete_onset)
 summary(cox_model_frailty)
 
 # coxme with random effects: rain to date + cumulative sun hours
-coxme_model <- coxme(Surv(dayOfWY, breeding_status) ~ 
+coxme_model <- coxme(Surv(dayOfWY, time2, breeding_status) ~ 
                      rain_to_date +
-                     sun_resid +
+                     # sun_resid +
                      # cum_sun_hours +
-                     # (1 | Watershed),
-                     (1 | LocationID),
+                     (1 | LocationID / BRDYEAR),
                    data = complete_onset)
 
 summary(coxme_model)
@@ -151,7 +153,7 @@ print(test_cox)
 
 # extract fixed effects and random effects of coxme model
 fixed_effects <- fixef(coxme_model)
-random_effects <- ranef(coxme_model)$LocationID
+random_effects <- ranef(coxme_model)$'LocationID/BRDYEAR'
 
 # get linear predictors of coxme model
 linear_pred <- predict(coxme_model, type = "lp")
@@ -159,13 +161,41 @@ linear_pred <- predict(coxme_model, type = "lp")
 # get baseline survival curve using cox_model_no_random results
 baseline_surv <- survfit(cox_model_no_random)
 
-# modify survival curve for each LocationID
-surv_summary <- summary(baseline_surv, times = baseline_surv$time)
-survival_estimates <- outer(
-  surv_summary$surv,
-  exp(random_effects),
-  function(base, re) base^re
-)
+### part I'm having trouble with ###
+
+cumulative_hazard <- -log(baseline_surv$surv)
+
+# # Adjust the survival curve using the random effects and fixed effects
+# survival_estimates <- lapply(names(random_effects), function(cluster) {
+#   random_effect <- random_effects[cluster]
+#   
+#   # Survival curve equation
+#   survival_curve <- function(t) {
+#     # Find the cumulative hazard for the current time t
+#     h_t <- cumulative_hazard[baseline_surv$time <= t]
+#     
+#     # Survival probability: exp(-sum of hazards) * random effect term
+#     exp(-sum(h_t) * exp(fixed_effects %*% complete_onset$rain_to_date + random_effect))
+#   }
+#   
+#   # Apply the survival curve function across all time points
+#   survival_values <- sapply(baseline_surv$time, survival_curve)
+#   return(survival_values)
+# })
+# 
+# # Combine results into a data frame for plotting
+# survival_data <- data.frame(
+#   time = rep(baseline_surv$time, length(random_effects)),
+#   survival = unlist(survival_estimates),
+#   random_effect = rep(names(random_effects), each = length(baseline_surv$time))
+# )
+# 
+# # Generate plot data for a specific random effect location (e.g., "KC01/2012")
+# plot_data <- survival_data %>%
+#   filter(random_effect == "KC01/2012")
+
+
+### end troublesome part ###
 
 # generate data for plotting -- for future reference: dayOfWY only goes up to 180 because that's the last day of first breeding
 plot_data <- data.frame(
@@ -174,7 +204,8 @@ plot_data <- data.frame(
   random_effect = rep(names(random_effects), length(surv_summary$time))
 ) %>% 
   group_by(dayOfWY, random_effect) %>% 
-  summarize(survival = mean(survival), .groups = "drop")  #aggregate by dayOfWY
+  summarize(survival = mean(survival), .groups = "drop") %>%  #aggregate by dayOfWY
+  filter(random_effect == "KC01/2012")
 
 
 # plot!
@@ -208,3 +239,15 @@ ggplot(coefficients, aes(x = hazard_ratio, y = cov)) +
   geom_point() +
   labs(x = "Hazard Ratio", y = "Covariate") +
   theme_minimal()
+
+# eda plots -- trying to figure out patterns in the data
+
+ggplot(complete_onset, aes(x = dayOfWY, y = sun_resid)) + 
+  geom_smooth(aes(color = LocationID), alpha = 0.4) +
+  geom_point(data = complete_onset %>% filter(breeding_status == 1), alpha = 0.25)
+
+ggplot(data = complete_onset, aes(x = cum_sun_hours, y = breeding_status)) +
+  geom_smooth()
+
+ggplot(data = complete_onset, aes(x = rain_to_date, y = breeding_status)) +
+  geom_smooth()
