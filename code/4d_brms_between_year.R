b5c642804b18b1abe56510f86ff2238c3c4af4b1
Robin Hinks <robin.e.hinks@gmail.com>
2025-06-25 12:27:55 -0700

    created site and watershed RE plots

--- code/4d_brms_between_year.R
+++ code/4d_brms_between_year.R
@@ -77,11 +77,10 @@ mod.zi.no.salinity.linear <- brm(
 summary(mod.zi.no.salinity.linear, prob = 0.89)
 powerscale_sensitivity(mod.zi.no.salinity.linear)
 powerscale_plot_dens(mod.zi.no.salinity.linear)
-
 # plotting prior and posterior distributions
 
 get_variables(mod.zi.no.salinity.linear)
-
+#### prior and posterior plots ####
 prior_dist <- prior_draws(mod.zi.no.salinity.linear,
                           variable = c("b_BRDYEAR_scaled", 
                                        "b_mean_percent_water_scaled",
@@ -171,8 +170,7 @@ prior_post_plot <- ggplot(data = prior_post, aes(x = value, y = parameter, fill 
 prior_post_plot
 
 
-##### plots: mod.zi.no.salinity.linear #####
-# using marginaleffects
+##### sjPlot effects plots: mod.zi.no.salinity.linear #####
 
 pred <- predictions(mod.zi.no.salinity.linear, conf_level = 0.89, type = "prediction", ndraws = 10, re_formula = NA)
 pred <- get_draws(pred)
@@ -206,6 +204,195 @@ background_2 <- "#FF9505"
 
 main_color_3 <- "#0070CC"
 background_3 <- "#47ACFF"
+
+scaled_between_year_round <- scaled_between_year %>% 
+  mutate(across(where(is.double), ~ round(., digits = 2)))
+
+sjPlot_effects <- function(term, xlab, color, ylab = "Number of egg masses") {
+  min_val <- min(scaled_between_year_round[[paste0(term, "_scaled")]])
+  max_val <- max(scaled_between_year_round[[paste0(term, "_scaled")]])
+  
+  switch(color,
+         green = {
+           mc <- main_color
+           bg <- background
+         },
+         grey = {
+           mc <- "black"
+           bg <- "#54494B"
+         },
+         blue = {
+           mc <- main_color_3
+           bg <- background_3
+         })
+  plot_data <- as.data.frame(get_model_data(mod.zi.no.salinity.linear, type = "pred", terms = paste0(term, "_scaled [" , min_val, ":", max_val, ", by = 0.01]"), interval = "confidence", ci.lvl = 0.89)) %>% 
+    select(-group, -group_col) %>% 
+    mutate(unscaled = (x * col_sd[[term]]) + col_means[[term]])
+  
+  xlim <- c(round(min(plot_data$unscaled)), round(max(plot_data$unscaled)))
+  
+  ggplot(plot_data, aes(x = unscaled, y = predicted)) +
+    # point data for appendix plot
+    # geom_point(data = scaled_between_year, aes(x = .data[[term]], y = num_egg_masses), alpha = 0.5, color = bg) +
+    geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.3, fill = bg) +
+    geom_line(linewidth = 1, color = mc) +
+    theme_bw() +
+    labs(x = xlab, y = ylab) +
+    scale_x_continuous(limits = xlim) +
+    scale_y_continuous(limits = c(-1, 55))
+}
+canopy_plot <- sjPlot_effects("interpolated_canopy", "Percent canopy cover", "green")
+water_plot <- sjPlot_effects("mean_percent_water", "Percent open water", "green", " ")
+year_plot <- sjPlot_effects("BRDYEAR", "Breeding year", "grey", " ")
+sub_veg_plot <- sjPlot_effects("mean_percent_sub", "Percent submergent vegetation", "blue")
+lag_rain_plot <- sjPlot_effects("yearly_rain_lag", "Lagged yearly rain (cm)", "blue", " ")
+water_temp_plot <- sjPlot_effects("WaterTemp", "Water temperature (°C)", "blue", " ")
+yearly_rain_plot <- sjPlot_effects("yearly_rain", "Yearly rain (cm)", "green", " ")
+
+# interaction plot for water regime and yearly rain
+# directly calling ggpredict because sjPlot's wrapper (get_model_data) won't let me get interaction data at intervals of 0.01
+plot_data <- ggpredict(mod.zi.no.salinity.linear, 
+                       terms = c("yearly_rain_scaled [-1.58:1.58 by=0.01]", "water_regime"),
+                       ci.level = 0.89,
+                       interval = "confidence") %>% 
+  as.data.frame() %>% 
+  mutate(unscaled = (x * col_sd$yearly_rain) + col_means$yearly_rain)
+
+interaction_plot <- ggplot(plot_data, aes(x = unscaled, y = predicted)) +
+  # geom_point(data = scaled_between_year, aes(x = yearly_rain, y = num_egg_masses, color = water_regime), alpha = 0.5) +
+  geom_ribbon(aes(ymin = conf.low, ymax = conf.high, fill = group), alpha = 0.3) +
+  geom_line(linewidth = 1, aes(color = group)) +
+  theme_bw() +
+  labs(x = "Yearly rain (cm)", y = "Number of egg masses", color = "Water regime", fill = "Water regime") +
+  scale_y_continuous(limits = c(0, 25)) +
+  scale_color_manual(values = c("black", "#49741A")) +
+  scale_fill_manual(values = c("#54494B", "#7DC82D"))
+
+# 3 cols, 2 rows
+effects_plots <- cowplot::plot_grid(canopy_plot, water_plot, year_plot, sub_veg_plot, lag_rain_plot, water_temp_plot, 
+                                    nrow = 2, align = "hv")
+
+cowplot::plot_grid(effects_plots, interaction_plot,
+                   nrow = 2, rel_heights = c(2, 1))
+
+# geom_smooth()# using sjPlot
+posterior <- as.matrix(mod.zi.no.salinity.linear)
+palette_green <- c(
+  "#A5DD69",
+  "#87D237",
+  "#68A626",
+  "#49741A",
+  "#2A430F",
+   "black")
+
+color_scheme_set(palette_green)
+
+#### forest plot for appendix ####
+mcmc_intervals(posterior, point_est = "mean", prob = 0.89, prob_outer = 0.89,
+               inner_size = 1, 
+               point_size = 2,
+               pars = c(
+                 # "b_water_flowlentic",
+                 "b_yearly_rain_scaled:water_regimeseasonal",
+                 "b_mean_percent_sub_scaled",
+                 "b_yearly_rain_lag_scaled",
+                 "b_water_regimeseasonal",
+                 "b_yearly_rain_scaled",
+                 "b_WaterTemp_scaled",
+                 "b_yearly_rain_lag_scaled:water_regimeseasonal",
+                 "b_BRDYEAR_scaled",
+                 "b_mean_percent_water_scaled",
+                 "b_interpolated_canopy_scaled",
+                 "b_water_flowlotic"                                
+                         
+               )) +
+  geom_vline(xintercept = 0, linetype = 2) +
+  scale_y_discrete(labels = c(
+    "b_BRDYEAR_scaled" = "Breeding year",
+    # "b_BRDYEAR_uncentered" = "Breeding year",
+    "b_mean_percent_water_scaled" = "Mean percent water",
+    "b_interpolated_canopy_scaled" = "Interpolated canopy cover",
+    "b_WaterTemp_scaled" = "Water temperature",
+    "b_mean_percent_sub_scaled" = "Mean percent submergent veg.",
+    "b_yearly_rain_scaled" = "Yearly rain",
+    "b_yearly_rain_lag_scaled" = "Lagged yearly rain",
+    "b_water_regimeseasonal" = "Seasonal water regime",
+    # "b_water_flowlentic" = "Lentic flow",
+    "b_water_flowlotic" = "Lotic flow",
+    "b_yearly_rain_scaled:water_regimeseasonal" = "Yearly rain × Seasonal water",
+    "b_yearly_rain_lag_scaled:water_regimeseasonal" = "Lagged rain × Seasonal water"
+  )) +
+  labs(x = "Estimate") +
+  theme_minimal()
+
+# plot_model(mod.zi.no.salinity.linear, type = "pred", terms = c("yearly_rain_lag_scaled"))
+
+
+
+##### OLD MODELS
+
+# hurdle GAM model no salinity
+#### plotting random effects -- watersheds ####
+# intercepts <- as.data.frame(ranef(mod.zi.no.salinity.linear)$Watershed) %>% 
+#   mutate(Watershed = rownames(.))
+# colnames(intercepts) <- str_remove_all(colnames(intercepts), ".Intercept")
+
+re <- as.matrix(mod.zi.no.salinity.linear) %>% 
+  as.data.frame() %>% 
+  pivot_longer(cols = everything(), names_to = "parameter", values_to = "value") %>% 
+  filter(str_detect(parameter, "(r_Watershed)\\[")) %>% 
+  mutate(parameter = str_remove_all(parameter, "r_Watershed\\[|,Intercept\\]"),
+         parameter = str_replace(parameter, "[.]", " ")) %>% 
+  rename(Watershed = parameter) %>% 
+  group_by(Watershed) %>% 
+  mutate(mean = mean(value),
+         lower = quantile(value, 0.055),
+         upper = quantile(value, 0.945)) %>% # 89% CIs
+  ungroup() %>% 
+  arrange(desc(mean)) %>% 
+  mutate(Watershed = fct_inorder(Watershed, ordered = T))
+
+levels(re$Watershed)
+
+ggplot(re, aes(x = value, y = Watershed)) +
+  geom_density_ridges(alpha = 0.7, rel_min_height = 0.01, scale = 0.8) +
+  geom_point(aes(x = mean)) +
+  geom_linerange(aes(xmin = lower, xmax = upper)) +
+  scale_x_continuous(limits = c(-5, 5)) +
+  scale_y_discrete(expand = expansion(mult = c(0.01, 0.06))) +
+  labs(x = "Distribution") +
+  theme_ridges(center_axis_labels = TRUE)
+#### plotting random effects -- sites within watersheds ####
+# intercepts <- as.data.frame(ranef(mod.zi.no.salinity.linear)$Watershed) %>% 
+#   mutate(Watershed = rownames(.))
+# colnames(intercepts) <- str_remove_all(colnames(intercepts), ".Intercept")
+
+re <- as.matrix(mod.zi.no.salinity.linear) %>% 
+  as.data.frame() %>% 
+  pivot_longer(cols = everything(), names_to = "parameter", values_to = "value") %>% 
+  filter(str_detect(parameter, "(r_Watershed)\\:")) %>% 
+  mutate(parameter = str_remove_all(parameter, "r_Watershed:LocationID\\[|,Intercept\\]"),
+         parameter = str_replace(parameter, "[.]", " ")) %>% 
+  # rename(Watershed = parameter) %>% 
+  separate_wider_delim(parameter,  delim = "_", names = c("Watershed", "Site")) %>% 
+  group_by(Watershed, Site) %>% 
+  mutate(mean = mean(value),
+         lower = quantile(value, 0.055),
+         upper = quantile(value, 0.945)) %>% # 89% CIs
+  ungroup() %>% 
+  arrange(desc(Watershed), desc(mean)) %>% 
+  mutate(Site = fct_inorder(Site, ordered = T),
+         Watershed = fct(Watershed))
+  
+
+ggplot(re, aes(x = value, y = Site)) +
+  geom_density_ridges(alpha = 0.7, rel_min_height = 0.01, scale = 0.8, aes(fill = Watershed)) +
+  geom_point(aes(x = mean)) +
+  geom_linerange(aes(xmin = lower, xmax = upper)) +
+  scale_x_continuous(limits = c(-8, 8)) +
+  scale_y_discrete(expand = expansion(mult = c(0.01, 0.06))) +
+  labs(x = "Distribution") +
+  theme_ridges(center_axis_labels = TRUE)
 
 #### marginaleffects by hand plots -- old ####
 # canopy -- significant
@@ -281,135 +468,6 @@ rain_water_regime_plot <- ggplot(pred_unscaled, aes(x = rain_unscaled, y = estim
   labs(x = "Yearly rainfall (cm)", y = "Number of egg masses") +
   facet_wrap(~water_regime, labeller = labeller(water_regime = c(perennial = "Perennial Water Regime", seasonal = "Seasonal Water Regime")))
 rain_water_regime_plot
-
-#### trying to use sjPlot for effects plots ####
-scaled_between_year_round <- scaled_between_year %>% 
-  mutate(across(where(is.double), ~ round(., digits = 2)))
-
-sjPlot_effects <- function(term, xlab, color, ylab = "Number of egg masses") {
-  min_val <- min(scaled_between_year_round[[paste0(term, "_scaled")]])
-  max_val <- max(scaled_between_year_round[[paste0(term, "_scaled")]])
-  
-  switch(color,
-         green = {
-           mc <- main_color
-           bg <- background
-         },
-         grey = {
-           mc <- "black"
-           bg <- "#54494B"
-         },
-         blue = {
-           mc <- main_color_3
-           bg <- background_3
-         })
-  plot_data <- as.data.frame(get_model_data(mod.zi.no.salinity.linear, type = "pred", terms = paste0(term, "_scaled [" , min_val, ":", max_val, ", by = 0.01]"), interval = "confidence")) %>% 
-    select(-group, -group_col) %>% 
-    mutate(unscaled = (x * col_sd[[term]]) + col_means[[term]])
-  
-  xlim <- c(round(min(plot_data$unscaled)), round(max(plot_data$unscaled)))
-  
-  ggplot(plot_data, aes(x = unscaled, y = predicted)) +
-    # point data for appendix plot
-    # geom_point(data = scaled_between_year, aes(x = .data[[term]], y = num_egg_masses), alpha = 0.5, color = bg) +
-    geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.3, fill = bg) +
-    geom_line(linewidth = 1, color = mc) +
-    theme_bw() +
-    labs(x = xlab, y = ylab) +
-    scale_x_continuous(limits = xlim) +
-    scale_y_continuous(limits = c(-1, 55))
-}
-canopy_plot <- sjPlot_effects("interpolated_canopy", "Percent canopy cover", "green")
-water_plot <- sjPlot_effects("mean_percent_water", "Percent open water", "green", " ")
-year_plot <- sjPlot_effects("BRDYEAR", "Breeding year", "grey", " ")
-sub_veg_plot <- sjPlot_effects("mean_percent_sub", "Percent submergent vegetation", "blue")
-lag_rain_plot <- sjPlot_effects("yearly_rain_lag", "Lagged yearly rain (cm)", "blue", " ")
-water_temp_plot <- sjPlot_effects("WaterTemp", "Water temperature (°C)", "blue", " ")
-yearly_rain_plot <- sjPlot_effects("yearly_rain", "Yearly rain (cm)", "green", " ")
-
-# interaction plot for water regime and yearly rain
-# directly calling ggpredict because sjPlot's wrapper (get_model_data) won't let me get interaction data at intervals of 0.01
-plot_data <- ggpredict(mod.zi.no.salinity.linear, 
-                       terms = c("yearly_rain_scaled [-1.58:1.58 by=0.01]", "water_regime"),
-                       ci.level = 0.89,
-                       interval = "confidence") %>% 
-  as.data.frame() %>% 
-  mutate(unscaled = (x * col_sd$yearly_rain) + col_means$yearly_rain)
-
-interaction_plot <- ggplot(plot_data, aes(x = unscaled, y = predicted)) +
-  # geom_point(data = scaled_between_year, aes(x = yearly_rain, y = num_egg_masses, color = water_regime), alpha = 0.5) +
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high, fill = group), alpha = 0.3) +
-  geom_line(linewidth = 1, aes(color = group)) +
-  theme_bw() +
-  labs(x = "Yearly rain (cm)", y = "Number of egg masses", color = "Water regime", fill = "Water regime") +
-  scale_y_continuous(limits = c(0, 25)) +
-  scale_color_manual(values = c("black", "#49741A")) +
-  scale_fill_manual(values = c("#54494B", "#7DC82D"))
-
-# 3 cols, 2 rows
-effects_plots <- cowplot::plot_grid(canopy_plot, water_plot, year_plot, sub_veg_plot, lag_rain_plot, water_temp_plot, 
-                                    nrow = 2, align = "hv")
-
-cowplot::plot_grid(effects_plots, interaction_plot,
-                   nrow = 2, rel_heights = c(2, 1))
-
-# geom_smooth()# using sjPlot
-posterior <- as.matrix(mod.zi.no.salinity.linear)
-palette_green <- c(
-  "#A5DD69",
-  "#87D237",
-  "#68A626",
-  "#49741A",
-  "#2A430F",
-   "black")
-
-color_scheme_set(palette_green)
-
-# forest plot
-mcmc_intervals(posterior, point_est = "mean", prob = 0.89, prob_outer = 0.89,
-               inner_size = 1, 
-               point_size = 2,
-               pars = c(
-                 # "b_water_flowlentic",
-                 "b_yearly_rain_scaled:water_regimeseasonal",
-                 "b_mean_percent_sub_scaled",
-                 "b_yearly_rain_lag_scaled",
-                 "b_water_regimeseasonal",
-                 "b_yearly_rain_scaled",
-                 "b_WaterTemp_scaled",
-                 "b_yearly_rain_lag_scaled:water_regimeseasonal",
-                 "b_BRDYEAR_scaled",
-                 "b_mean_percent_water_scaled",
-                 "b_interpolated_canopy_scaled",
-                 "b_water_flowlotic"                                
-                         
-               )) +
-  geom_vline(xintercept = 0, linetype = 2) +
-  scale_y_discrete(labels = c(
-    "b_BRDYEAR_scaled" = "Breeding year",
-    # "b_BRDYEAR_uncentered" = "Breeding year",
-    "b_mean_percent_water_scaled" = "Mean percent water",
-    "b_interpolated_canopy_scaled" = "Interpolated canopy cover",
-    "b_WaterTemp_scaled" = "Water temperature",
-    "b_mean_percent_sub_scaled" = "Mean percent submergent veg.",
-    "b_yearly_rain_scaled" = "Yearly rain",
-    "b_yearly_rain_lag_scaled" = "Lagged yearly rain",
-    "b_water_regimeseasonal" = "Seasonal water regime",
-    # "b_water_flowlentic" = "Lentic flow",
-    "b_water_flowlotic" = "Lotic flow",
-    "b_yearly_rain_scaled:water_regimeseasonal" = "Yearly rain × Seasonal water",
-    "b_yearly_rain_lag_scaled:water_regimeseasonal" = "Lagged rain × Seasonal water"
-  )) +
-  labs(x = "Estimate") +
-  theme_minimal()
-
-# plot_model(mod.zi.no.salinity.linear, type = "pred", terms = c("yearly_rain_lag_scaled"))
-
-
-
-##### OLD MODELS
-
-# hurdle GAM model no salinity
 ##### priors: bprior.no.sal #######
 bprior.no.sal <- c(
   #counts
